7ff55442a1cd1e77f0ce80e9b8775986ef6f9b3c
Dima Ryskin <dima.ryskin@gmail.com>
2020-11-03 09:11:48 +0200

    fix torrent blackhole bug caused by inverse if logic

--- blackhole_torrent_uploader.sh
+++ blackhole_torrent_uploader.sh
@@ -2,17 +2,18 @@
 echo "Starting Blackhole Torrent Uploader at $TORRENT_BLACKHOLE_DIR";
 inotifywait -m "$TORRENT_BLACKHOLE_DIR" -e create -e moved_to |
     while read path action file; do
-        
-        if [[ "$file" =~ .*torrent$ ]]; then # Does the file end with .torrent?
-            echo "The file is not a torrent. Skipping [ $file ]"; 
-        else
-            echo "Uploading $path$file";
-            curl -X POST "https://upload.put.io/v2/files/upload" \
-                  -H "accept: application/json" \
-                  -H "Authorization: Bearer $PUTIO_AUTH_TOKEN" \
-                  -H "Content-Type: multipart/form-data" \
-                  -F "filename=" \
-                  -F "parent_id=$PUTIO_PARENT_ID" \
-                  -F "file=@$path$file"
+    
+        if [[ "$file" =~ .*torrent$ ]]  # Does the file end with .torrent?
+            then
+                echo "Uploading $path$file"
+                curl -X POST "https://upload.put.io/v2/files/upload" \
+                    -H "accept: application/json" \
+                    -H "Authorization: Bearer $PUTIO_AUTH_TOKEN" \
+                    -H "Content-Type: multipart/form-data" \
+                    -F "filename=" \
+                    -F "parent_id=$PUTIO_PARENT_ID" \
+                    -F "file=@$path$file"
+            else
+                echo "The file is not a torrent. Skipping [ $file ]"
         fi
     done
